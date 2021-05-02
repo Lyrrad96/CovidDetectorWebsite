50da8d7665cea1bb52d68a21d9bbd476103cc531
Lyrrad96 <56973672+Lyrrad96@users.noreply.github.com>
2021-05-02 10:38:38 +0530

    Update covdetector.py
    changed complete code

--- covdetector.py
+++ covdetector.py
@@ -1,45 +1,52 @@
 import streamlit as st
-import time
+st.set_page_config(layout="centered")
 import numpy as np
 import streamlit.components.v1 as components
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-import cv2
 import tensorflow as tf
 from keras.preprocessing import image
-import os
 
 
-model = tf.keras.models.load_model("/content/drive/MyDrive/Detection/model_adv.h5")
+HtmlFile = open("/content/drive/MyDrive/Detection/home.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+print(source_code)
+components.html(source_code)
 
-categories = ["Covid", "Normal"]  # will use this to convert prediction num to string value
+model = tf.keras.models.load_model("/content/drive/MyDrive/Detection/model.h5")
 
-uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg'])
+  # will use this to convert prediction num to string value
+
+#uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg'])
 # Using PIL
-from PIL import Image
-img= np.asarray(Image.open(uploaded_file))
+#from PIL import Image
+#img= np.asarray(Image.open(uploaded_file))
 
 #img = np.asarray(img)
-st.image(img, caption='Uploaded Image.')
+#st.image(img, caption='Uploaded Image.')
 
-#img = image.load_img('/content/drive/MyDrive/DataSet/Val/Covid/10.1016-slash-j.anl.2020.04.002-a.png', target_size=(224, 224))
 
+#load image
+img = image.load_img('/content/drive/MyDrive/DataSet/Test/Normal/Normal-292.png', target_size=(224, 224))
+
+#convert to numpy array
 x = image.img_to_array(img)
-st.write(x)
-#st.image(x, caption='Uploaded Image.')
 x = np.expand_dims(x, axis=0)
-st.write(x)
 
 images = np.vstack([x])
 images = np.divide(images, 255)
+#print image
 st.image(images, caption='Uploaded Image.')
 
+#make prediction
 pred = model.predict(images)
 
-#st.write(pred)
-
+pred[0][0]=round(pred[0][0])
+#declare categories
+categories = ["Positive", "Negative"]
+#display result
 string="Result: "+categories[int(pred[0][0])]
-
-st.success(string)
+st.write(pred[0][0])
+if pred[0][0] == 0:
+  st.error(string)
+else:
+  st.success(string)
